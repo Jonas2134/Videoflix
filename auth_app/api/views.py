c42bd82f0534492fc35bb3f05391ac215256e90e
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-08-14 16:34:05 +0200

    finish ActivateAccountView

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -43,4 +43,17 @@ class RegisterView(generics.CreateAPIView):
 
 
 class ActivateAccountView(generics.GenericAPIView):
-    pass
+    permission_classes = [AllowAny]
+
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response({"message": "Account activated successfully."}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid activation link."}, status=status.HTTP_400_BAD_REQUEST)
