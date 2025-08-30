e89c3337065e310f54b1304996fb396aac45f7cf
Jonas Stiefer <neo.stief.jon@gmail.com>
2025-08-31 01:30:15 +0200

    Apply .gitignore and remove ignored files from repo

--- core/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('auth_app.api.urls')),
-    path('', include('video_app.api.urls')),
-]
--- video_app/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import VideoListView, video_hls
-
-urlpatterns = [
-    path('video/', VideoListView.as_view(), name='video-list'),
-    path('video/<int:movie_id>/<str:resolution>/index.m3u8', video_hls),
-    path('video/<int:movie_id>/<str:resolution>/<str:segment>', video_hls),
-]
